a21a14ffd889e07aaab13a302a696a0f23ebea78
Filip Riha <riha.fina@gmail.com>
2022-06-19 14:05:14 +0200

    Implement DependencyGraph

--- src/Depender/DependencyGraph.hs
+++ src/Depender/DependencyGraph.hs
@@ -1,4 +1,56 @@
-module Depender.DependencyGraph where
+module Depender.DependencyGraph
+  ( -- * Grap types
+    Name,
+    Dependencies,
+    Graph,
 
--- | Dummy Graph type
-data Graph = DummyGraph
+    -- * New graph
+    empty,
+
+    -- * Manipulate graph
+    addDependecies,
+    addVertex,
+
+    -- * Extract verticies
+    toList,
+  )
+where
+
+import qualified Data.Bifunctor as Bf
+import Data.List (nub)
+import qualified Data.Map as Map
+
+-- | Simple alias for module name
+type Name = String
+
+-- | Dependencies of module
+type Dependencies = [Name]
+
+-- | Graph type - map from module to it's dependencies
+newtype Graph = MkGraph (Map.Map Name Dependencies)
+
+-- | New empty graph
+empty :: Graph
+empty = MkGraph Map.empty
+
+-- | Add an isolated vertex to graph or do nothing if it is already present
+addVertex :: Graph -> Name -> Graph
+addVertex (MkGraph g) name = MkGraph $ insertW g
+  where
+    -- the consts keeps the previous value
+    insertW = Map.insertWith const name []
+
+-- | Add dependencies to a vertex, add the vertex if it is not present and
+-- add the dependencies as new verticies if they're not present
+addDependecies :: Graph -> Name -> Dependencies -> Graph
+addDependecies (MkGraph g) name deps =
+  flip (foldl addVertex) deps $
+    MkGraph $
+      Map.insertWith (++) name deps g
+
+-- | Simple alias on one vertex
+type Vertex = (Name, Dependencies)
+
+-- | Return a list of verticies and their dependencies
+toList :: Graph -> [Vertex]
+toList (MkGraph g) = map (Bf.second nub) $ Map.toList g
